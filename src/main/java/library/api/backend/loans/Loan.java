7940e297f52b445128800ec3937c04f3f8ca1750
kevin lambe <kevin.lambe@teamaolcom>
2017-04-05 16:04:34 +0100

    added mysql functionality to programme all entities persisted to database

---
+++ src/main/java/library/api/backend/loans/Loan.java
@@ -0,0 +1,23 @@
+package library.api.backend.loans;
+
+import library.api.backend.books.Book;
+import library.api.backend.users.User;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+//
+//@Entity
+//@Table(name="users")
+public class Loan {
+
+//    @Id
+//    int id;
+//
+//
+//    Book book;
+//    User user;
+//
+//    String initialLoanDate;
+//    String dueDate;
+}
