adcd58e001f4861ef3a969f823684f71c9a9fde1
Kevin <klambe17@gmail.com>
2017-04-09 17:16:02 +0100

    updated controller for loan

--- src/main/java/library/api/backend/loans/LoanController.java
+++ src/main/java/library/api/backend/loans/LoanController.java
@@ -1,7 +1,40 @@
 package library.api.backend.loans;
 
-/**
- * Created by cintec on 06/04/2017.
- */
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
 public class LoanController {
+
+    @Autowired
+    private LoanService myLoanService;
+
+    @RequestMapping(value="/loans", method = RequestMethod.GET)
+    public List<Loan> getAllLoans(){return myLoanService.getAllLoans();
+    }
+
+    @RequestMapping(value="/loans/{id}", method = RequestMethod.GET)
+    public Loan getLoanById(@PathVariable int id){return myLoanService.getLoan(id);
+    }
+
+    @RequestMapping(value="/loans", method = RequestMethod.POST)
+    public void addLoan(Loan loan){ myLoanService.addLoan(loan);
+    }
+    @RequestMapping(value="/loans", method = RequestMethod.PUT)
+    public void updateLoan(Loan loan){ myLoanService.updateLoan(loan);
+    }
+
+    @RequestMapping(value="/loans/{id}", method = RequestMethod.DELETE)
+    public void updateLoan(@PathVariable int id){ myLoanService.deleteLoan(id);
+    }
+
 }
+
--- src/main/java/library/api/backend/loans/LoanService.java
+++ src/main/java/library/api/backend/loans/LoanService.java
@@ -1,7 +1,31 @@
 package library.api.backend.loans;
 
-/**
- * Created by cintec on 06/04/2017.
- */
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class LoanService {
+
+    @Autowired
+    private LoanRepository myLoanRepository;
+
+    public List<Loan> getAllLoans(){
+        List<Loan> myLoans = new ArrayList<>();
+
+        myLoanRepository.findAll().forEach(myLoans::add);
+        return myLoans;
+
+    }
+
+    public Loan getLoan(int id){return myLoanRepository.findOne(id);}
+
+    public void addLoan(Loan loan){myLoanRepository.save(loan);}
+    public void updateLoan(Loan loan){myLoanRepository.save(loan);}
+    public void deleteLoan(int id){myLoanRepository.delete(id);}
+
+
 }
